0792eb1e25e594608059151e3d69e2305e42552b
Rafaa Omar <rafaa.ommar@gmail.com>
2025-11-29 04:31:34 +0300

    middlewares

--- UserManagementAPI/Program.cs
+++ UserManagementAPI/Program.cs
@@ -1,13 +1,15 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 builder.Services.AddControllers();// Add services for controllers
 builder.Services.AddScoped<IUserService, UserService>(); // Register UserService 
+builder.Logging.AddConsole();//add console logging 
 
 var app = builder.Build();
-app.UseMiddleware<GlobalMiddleware>();
+//Middleware registration
+app.UseMiddleware<GlobalMiddleware>();//first middleware to handle global exceptions
+app.UseMiddleware<UserManagement.Middlewares.AuthenticationMiddleware>();//second middleware to authenticate requests
+app.UseMiddleware<UserManagement.Middlewares.LoggingMiddleware>();//third middleware to log requests & responses
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
