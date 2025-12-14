c5a05a8daec25e3831a3fb9f4635601c332ef53c
Rafaa Omar <rafaa.ommar@gmail.com>
2025-12-14 04:50:43 +0300

    problem solved

---
+++ resCoffeeNight/MyCoffeeNight/BlazorUI/Services/CartService.cs
@@ -0,0 +1,24 @@
+using BlazorUI.Models;
+namespace BlazorUI.Services{
+public class CartService
+{
+    public List<CartItem> Items{get;set;} = new();
+
+    public void AddToCart(CartItem item)
+    {
+        Items.Add(item);
+    }
+    public void RemoveFromCart(CartItem item)
+    {
+        Items.Remove(item);
+    }
+    public int GetTotal()
+    {
+        return Items.Sum(i => i.TotalPrice);
+    }
+    public void ClearCart()
+    {
+        Items.Clear();
+    }
+}
+}
