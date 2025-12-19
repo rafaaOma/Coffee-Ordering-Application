d63895d13864075fc524383f967fdf7363ae99ca
Rafaa Omar <rafaa.ommar@gmail.com>
2025-12-19 05:08:32 +0300

    updating css files

--- Models/CartItem.cs
+++ Models/CartItem.cs
@@ -4,6 +4,9 @@ namespace BlazorUI.Models{
     {
         public string CoffeeName{get; set;}
         public string Size{get;set;}
+        public int SmallPrice { get; set; }
+        public int MediumPrice { get; set; }
+        public int LargePrice { get; set; }
         public int TotalPrice{get;set;}
         public int Quantity{get;set;}
         public string ImageCart { get; set; }
